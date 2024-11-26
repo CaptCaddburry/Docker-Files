bd7faa73ece3e04e815d417102d5ca167a2b61a5
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2024-11-26 14:36:18 -0500

    Update app.py
    Added new webpage to webapp

--- WebApp/web-app/app.py
+++ WebApp/web-app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/new')
+def new():
+    return render_template('new.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
